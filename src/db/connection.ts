92832acba37fd724e80c3c939323ffc95342a2d4
jdeluna06 <jdeluna0672@gmail.com>
2024-09-03 15:34:14 -0700

    added node modules

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,13 +1,27 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+//Import.and.require .Pool (node-postgres)
+// We'll be creating a Connection Pool. Read up on the benefits here:
 import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-  host: "localhost",
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  port: 5432,
+user: process.env.DB_USER,
+password: process.env.DB_PASSWORD,
+host: 'localhost',
+database: process.env.DB_NAME,
+port: 5432,
 });
 
+const connectToDb = async () => {
+  try {
+    await pool.connect();
+    console.log('Connected to the database');
+  } catch (error) {
+    console.error('Error connecting to the database: ', err);
+    process.exit(1);
+  }
+};
+
+export { pool, connectToDb };
